86e973e2bcd35310d49cfbe671b7fe72853b55df
Matheus Mariano <vmatheus.mariano@gmail.com>
2017-07-20 20:32:34 -0300

    Add Twitter and Instagram to report test

--- tests/Feature/Api/ReportControllerTest.php
+++ tests/Feature/Api/ReportControllerTest.php
@@ -11,6 +11,8 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Models\User;
 use App\Models\SocialProfile;
 use App\Models\FacebookSnapshot;
+use App\Models\TwitterSnapshot;
+use App\Models\InstagramSnapshot;
 
 class ReportControllerTest extends TestCase
 {
@@ -18,23 +20,59 @@ class ReportControllerTest extends TestCase
 
     public function testReportController()
     {
-        $socialProfile = factory(SocialProfile::class)
-            ->states('facebook')
-            ->create();
+        $user = factory(User::class)->create();
+
+        $facebookProfile = $user->socialProfiles()->save(
+            factory(SocialProfile::class)
+                ->states('facebook')
+                ->make()
+        );
+
+        $twitterProfile = $user->socialProfiles()->save(
+            factory(SocialProfile::class)
+                ->states('twitter')
+                ->make()
+        );
+
+        $instagramProfile = $user->socialProfiles()->save(
+            factory(SocialProfile::class)
+                ->states('instagram')
+                ->make()
+        );
 
         foreach (range(0, 4) as $index) {
-            $socialProfile->facebookSnapshots()->save(
+            $facebookProfile->facebookSnapshots()->save(
                 factory(FacebookSnapshot::class)->make([
                     'likes' => 50 * $index,
                     'created_at' => Carbon::now()->subDays(4 - $index)->startOfDay(),
                     'updated_at' => Carbon::now()->subDays(4 - $index)->startOfDay(),
                 ])
             );
+
+            $twitterProfile->twitterSnapshots()->save(
+                factory(TwitterSnapshot::class)->make([
+                    'followers' => 25 * $index,
+                    'following' => 30 * $index,
+                    'created_at' => Carbon::now()->subDays(4 - $index)->startOfDay(),
+                    'updated_at' => Carbon::now()->subDays(4 - $index)->startOfDay(),
+                ])
+            );
+
+            $instagramProfile->instagramSnapshots()->save(
+                factory(InstagramSnapshot::class)->make([
+                    'followers' => 40 * $index,
+                    'following' => 42 * $index,
+                    'created_at' => Carbon::now()->subDays(4 - $index)->startOfDay(),
+                    'updated_at' => Carbon::now()->subDays(4 - $index)->startOfDay(),
+                ])
+            );
         }
 
+        // Facebook profile
+
         $response = $this
-            ->actingAs($socialProfile->user)
-            ->json('GET', 'api/social_profile/' . $socialProfile->id . '/report', [
+            ->actingAs($facebookProfile->user)
+            ->json('GET', 'api/social_profile/' . $facebookProfile->id . '/report', [
                 'start_date' => Carbon::now()->subDays(3)->toDateString(),
                 'end_date' => Carbon::now()->subDays(1)->toDateString(),
             ]);
@@ -51,12 +89,56 @@ class ReportControllerTest extends TestCase
             json_decode($response->getContent(), true)
         );
 
-        // Wrong user
-        $user = factory(User::class)->create();
+        // Twitter profile
 
         $response = $this
-            ->actingAs($user)
-            ->json('GET', 'api/social_profile/' . $socialProfile->id . '/report', [
+            ->actingAs($twitterProfile->user)
+            ->json('GET', 'api/social_profile/' . $twitterProfile->id . '/report', [
+                'start_date' => Carbon::now()->subDays(3)->toDateString(),
+                'end_date' => Carbon::now()->subDays(1)->toDateString(),
+            ]);
+
+        $response->assertStatus(200);
+
+        $this->assertArraySubset(
+            array_map(function ($index) {
+                return [
+                    'followers' => 25,
+                    'following' => 30,
+                    'created_at' => Carbon::now()->subDays(4 - $index)->startOfDay()->toDateTimeString(),
+                ];
+            }, range(1, 3)),
+            json_decode($response->getContent(), true)
+        );
+
+        // Instagram profile
+
+        $response = $this
+            ->actingAs($instagramProfile->user)
+            ->json('GET', 'api/social_profile/' . $instagramProfile->id . '/report', [
+                'start_date' => Carbon::now()->subDays(3)->toDateString(),
+                'end_date' => Carbon::now()->subDays(1)->toDateString(),
+            ]);
+
+        $response->assertStatus(200);
+
+        $this->assertArraySubset(
+            array_map(function ($index) {
+                return [
+                    'followers' => 40,
+                    'following' => 42,
+                    'created_at' => Carbon::now()->subDays(4 - $index)->startOfDay()->toDateTimeString(),
+                ];
+            }, range(1, 3)),
+            json_decode($response->getContent(), true)
+        );
+
+        // Wrong user
+        $anotherUser = factory(User::class)->create();
+
+        $response = $this
+            ->actingAs($anotherUser)
+            ->json('GET', 'api/social_profile/' . $facebookProfile->id . '/report', [
                 'start_date' => Carbon::now()->subDays(3)->toDateString(),
                 'end_date' => Carbon::now()->subDays(1)->toDateString(),
             ]);
